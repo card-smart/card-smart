19c1d20fe3ad4dbe87d6a038e2d93ead26d9c29c
Veronika Hanulíková <xhanulik@fi.muni.cz>
2023-03-14 16:36:53 +0100

    Refactor filesystem class

--- applet/src/main/java/applet/FileSystem.java
+++ applet/src/main/java/applet/FileSystem.java
@@ -1,5 +1,9 @@
 package applet;
 
+import javacard.framework.JCSystem;
+import javacard.framework.Util;
+
+import static applet.CardSmartApplet.NAME_MAX_LEN;
 import static javacard.framework.Util.arrayCompare;
 
 public class FileSystem {
@@ -8,91 +12,166 @@ public class FileSystem {
      * FileSystem constants
      */
     private static final byte RECORDS_MAX_NUMBER = (byte) 16;
+    private static final short TEMP_ARRAY_LEN = (short) 256;
 
     /*
      * Records store in filesystem
      */
-    Record[] records;
-    byte numberOfRecords;
+    private final Record[] records;
+    private short numberOfRecords;
+    private final byte[] tempArray;
 
-    /*
-     * Create filesystem instance
+    /**
+     * Constructor for creating card filesystem
      */
     public FileSystem() {
         /* Allocate maximum number of records */
         records = new Record[RECORDS_MAX_NUMBER];
+        for (byte i = 0; i < RECORDS_MAX_NUMBER; i++) {
+            records[i] = new Record();
+        }
+
         numberOfRecords = 0;
+        /* Temporal array for storing values */
+        tempArray = JCSystem.makeTransientByteArray(TEMP_ARRAY_LEN, JCSystem.CLEAR_ON_DESELECT);
     }
 
-    /*
+    /**
+     * Get number of the non-empty records in the filesystem
+     *
+     * @return number of records in the filesystem
+     */
+    public short getNumberOfRecords() {
+        return numberOfRecords;
+    }
+
+    /**
      * Find index of first empty record slot
+     *
+     * @return index of the first empty record slot in the filesystem
      */
     private byte getIndexOfFirstEmptyRecord() {
         byte index;
         for (index = 0; index < RECORDS_MAX_NUMBER; index++) {
-            if (records[index].isEmpty() == 1) {
+            if (records[index].isEmpty() == 0) {
                 return index;
             }
         }
         return -1;
     }
 
-    /*
+    /**
      * Get index of record with given name
+     *
+     * @param name    A byte[] containing the name.
+     * @param nameLength The length of the name.
+     * @throw InvalidArgumentException   When name is Null.
+     * @throw StorageException When name is empty ("") and length is not 0.
      */
-    private byte getIndexByName(byte[] name, byte nameLen) {
-        byte index = 0;
-        for (index = 0; index < RECORDS_MAX_NUMBER; index++) {
-            if (arrayCompare(name, (short) 0, records[index].getName(), (short) 0, nameLen) == 0) {
+    private byte getIndexByName(byte[] name, byte nameLength) throws InvalidArgumentException, StorageException {
+        for (byte index = 0; index < RECORDS_MAX_NUMBER; index++) {
+            if (records[index].isEmpty() == 0)
+                continue;
+            byte len = records[index].getName(tempArray);
+            if (len == nameLength
+                    && arrayCompare(name, (short) 0, tempArray, (short) 0, nameLength) == 0) {
                 return index;
             }
         }
         return -1;
     }
 
-    /*
+    /**
      * Store name and secret in the first empty record slot
+     *
+     * @param name    A byte[] containing the name.
+     * @param nameLength The length of the name.
+     * @param secret  A byte[] containing the secret to store.
+     * @param secretLength The length of the secret
+     * @throw InvalidArgumentException
+     * @throw StorageException
      */
-    public void createRecord(byte[] name, byte nameLen, byte[] secret, byte secretLen) throws StorageException, InvalidArgumentException {
-        if (name == null || nameLen < 0
-                || secret == null || secretLen < 0) {
+    public void createRecord(byte[] name, byte nameLength, byte[] secret, byte secretLength) throws StorageException, InvalidArgumentException {
+        if (name == null || nameLength < 0
+                || secret == null || secretLength < 0) {
             throw new InvalidArgumentException("Invalid arguments when creating record");
         }
 
         byte index = getIndexOfFirstEmptyRecord();
-        if (index < -1) {
+        if (index < 0) {
             throw new StorageException("Storage full");
         }
-        records[index].initRecord(name, nameLen, secret, secretLen);
+        records[index].initRecord(name, nameLength, secret, secretLength);
+        numberOfRecords++;
     }
 
-    public void deleteRecord(byte[] name, byte nameLen) throws InvalidArgumentException, StorageException {
-        if (nameLen < 0) {
+
+    /**
+     * Delete record with given name if it exists
+     *
+     * @param name    A byte[] containing the name.
+     * @param nameLength The length of the name.
+     * @throw InvalidArgumentException
+     * @throw StorageException
+     */
+    public void deleteRecord(byte[] name, byte nameLength) throws InvalidArgumentException, StorageException {
+        if (nameLength < 0) {
             throw new InvalidArgumentException("Invalid length of name");
         }
 
         /* Find index of given record */
-        byte index = getIndexByName(name, nameLen);
+        byte index = getIndexByName(name, nameLength);
         if (index < 0) {
             throw new StorageException("No record with given name exists");
         }
         records[index].eraseRecord();
+        numberOfRecords--;
     }
 
-    /*
+    /**
      * Fill output buffer with value of secret
+     *
+     * @param name    A byte[] containing the name.
+     * @param nameLength The length of the name.
+     * @param outputBuffer A byte[] output buffer where the secret is stored.
+     * @throw InvalidArgumentException
+     * @throw StorageException
      */
-    public byte getSecretByName(byte[] name, byte nameLen, byte[] outBuffer, byte bufferLen) throws InvalidArgumentException, ConsistencyException {
-        if (name == null || nameLen < 0
-                || outBuffer == null || bufferLen < 0) {
+    public short getSecretByName(byte[] name, byte nameLength, byte[] outputBuffer) throws InvalidArgumentException, ConsistencyException, StorageException {
+        if (name == null || nameLength < 0
+                || outputBuffer == null || outputBuffer.length == 0) {
             throw new InvalidArgumentException("Invalid arguments when getting secret by name");
         }
 
-        byte index = getIndexByName(name, nameLen);
-        return records[index].getSecret(outBuffer, bufferLen);
+        byte index = getIndexByName(name, nameLength);
+        if (index < 0) {
+            return 0;
+        }
+
+        return records[index].getSecret(outputBuffer);
     }
 
-    public void getAllNames(byte[] outBuffer, byte bufferLen) {
-        // TODO
+    /**
+     * Fill output buffer wth concatenated names
+     *
+     * @param outputBuffer    A byte[] containing the name
+     * @throw InvalidArgumentException
+     * @throw StorageException
+     * @return length of concatenated names and their lengths
+     * */
+    public short getAllNames(byte[] outputBuffer) throws InvalidArgumentException, StorageException {
+        if (outputBuffer.length < RECORDS_MAX_NUMBER * NAME_MAX_LEN) {
+            throw new InvalidArgumentException("Output buffer should have maximal possible length.");
+        }
+        short offset = 0;
+        for (int index = 0; index < RECORDS_MAX_NUMBER; index++) {
+            if (records[index].isEmpty() == 0)
+                continue;
+            byte len = records[index].getName(tempArray);
+            outputBuffer[offset] = len;
+            Util.arrayCopyNonAtomic(this.tempArray, (short) 0, outputBuffer, (short) (offset + 1), len);
+            offset += 1 + len;
+        }
+        return 0;
     }
 }
